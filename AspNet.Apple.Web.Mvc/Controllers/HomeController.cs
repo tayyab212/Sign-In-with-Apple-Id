401a668e9702a074c075dec83c752e31f31e4f08
tayyab212 <tthunjra@gmail.com>
2021-06-16 22:07:12 +0500

    Add project files.

---
+++ AspNet.Apple.Web.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNet.Apple.Web.Mvc.Models;
+using Microsoft.Extensions.Options;
+
+namespace AspNet.Apple.Web.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppSettings _appSettings;
+
+        public HomeController(ILogger<HomeController> logger, IOptions<AppSettings> appSettings)
+        {
+            _logger = logger;
+            _appSettings = appSettings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            HomeViewModel homeViewModel = new HomeViewModel()
+            {
+                ClientId = _appSettings.ClientId,
+                RedirectUri = _appSettings.RedirectUri
+            };
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
